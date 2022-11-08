850d65273d274337c357529acb213c3c7d6f1d5f
Felipe Ojeda <felipe.ojeda19@gmail.com>
2022-11-08 10:35:22 -0300

    fix ux

--- public/application/views/ExamView.php
+++ public/application/views/ExamView.php
@@ -5,6 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <base href="<?php echo base_url();?>">
 <head>
   
+
 	<meta charset="utf-8">
 	<title>Bienvenido a Examenes Scrum</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
@@ -26,9 +27,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
   </p>
   <form method="post" id="frm_answer" action="" name="frm_answer">
+  
+  <?php
 
-<?php
-//print_r($alternatives);
 
   foreach ($alternatives as $alternative)
   {
@@ -36,22 +37,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     if($alternativeselected == $alternative->idalternative)
     {
-?>
-
-
+  ?>
 
 <input type="radio" name="alternative" value="<?php echo $alternative->idalternative; ?>" checked>
 <label for="html"><?php echo $alternative->Alternative; ?></label><br>
 
-
 <?php
 
     }else{
-
-
-    
-
-
 
    ?>
 
@@ -93,9 +86,7 @@ if($isfinal)
 
 	</div>
 </form>
-<form action="ExamController/backquestion">
-    
-</form>
+
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
@@ -135,6 +126,57 @@ $( "#btn_end" ).click(function() {
 
 
 });
+
+<?php 
+
+//if is exam start create a cookie with the date to start
+
+if($isStart)
+{
+
+  echo "const date = new Date();";  
+  echo "date.setMinutes(date.getMinutes() + 30);";
+  echo "document.cookie = date;";
+
+}
+
+?>
+
+
+
+
+var countDownDate = new Date(document.cookie).getTime();
+
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("timerexam").innerHTML =   minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    $('#frm_answer').attr('action', 'ResultController/index');
+      $( "#frm_answer" ).submit();
+    document.getElementById("timerexam").innerHTML = "EXPIRED";
+  }
+}, 10);
+
+
+
 
   </script>
 
